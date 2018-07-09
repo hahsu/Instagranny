30c24233ecc8ef8eff3bf9cd181b0f1112e2b00c
Hannah Hsu <hahsu@fb.com>
2018-07-09 14:43:33 -0700

    Initial Commit

---
+++ Instagranny8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instagranny8
+//
+//  Created by Hannah Hsu on 7/9/18.
+//  Copyright © 2018 Hannah Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
